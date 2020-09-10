8f4b936c2be6b47f6630923673564d4f3ed089f1
linxueyu <lin2006yuo@gmail.com>
2020-09-10 10:20:34 +0800

    WIP: 丐版

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
-import React from "./react"
+import React from "./react/source"
 const ReactDOM = React
-
-// import ReactDOM from "./react"
-// import  React from 'react'
-
-const element = (
-  <div>
-    <header>标题</header>
-    <section>
-      <div>哈哈</div>
-      <div>哈哈</div>
-    </section>
-    <footer>底部</footer>
-  </div>
-)
 
 const App = () => {
   return (
     <div>
       <header>函数组件</header>
+      <div>
+        <header>标题</header>
+        <section>
+          <div>哈哈</div>
+          <div>哈哈</div>
+        </section>
+        <footer>底部</footer>
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(element, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
